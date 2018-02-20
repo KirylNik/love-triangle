27129f58ac1a9b6eb2df07891fe99385b5cfc44e
Kiryl Nikalaichuk <36420342+KirylNik@users.noreply.github.com>
2018-02-20 14:55:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,47 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences) {
+
+    let triangleCount = [];
+
+    for (let i = 1; i < preferences.length; i++) {
+        let a = preferences[i-1];
+        let b = preferences[a-1];
+        let c = preferences[b-1]
+        if (c == i) {
+            if (a != b && a != i) {
+                let triangleTemp = [];
+                triangleTemp.push(a);
+                triangleTemp.push(b);
+                triangleTemp.push(c);
+                triangleTemp.sort(compareNumeric);
+                /* Sort array  */
+                function compareNumeric(a, b) {
+                    return a - b;
+                }
+
+                triangleCount.push(triangleTemp);
+            }
+        } else {
+            continue;
+        }
+    }
+    /* Array to string */
+    for (let i = 0; i < triangleCount.length; i++) {
+        let triangleTempString = '' + triangleCount[i];
+        triangleCount[i] = triangleTempString;
+    };
+    /* String compare*/
+    for (let i = 0; i < triangleCount.length; i++) {
+        for (let y = i+1; y < triangleCount.length; y++) {
+
+            if (triangleCount[i] == triangleCount[y]) {
+                triangleCount.splice([y], 1);
+                y--;
+            }
+        }
+
+    };
+    return triangleCount.length;
 };
